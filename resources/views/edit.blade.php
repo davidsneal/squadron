b16f3fa6e6e662de20fbd9f0c9a6fd262ec33d4e
David Neal <drumming4eva@hotmail.com>
2015-02-28 13:19:01 +0000

    Initial Commit

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-2">
+			<form>
+				<input name="title" type="text" placeholder="Title?" />
+				<textarea name="content" data-provide="markdown" rows="10"></textarea>
+				<label class="checkbox">
+				<input name="publish" type="checkbox"> Publish
+				</label>
+				<hr/>
+				<button type="submit" class="btn">Submit</button>
+			</form>
+		</div>
+	</div>
+</div>
+@endsection
